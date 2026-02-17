d977ca8cdc6b02d495a6357113a04efac48542bd
dkhobbs <91768310+dkhobbs@users.noreply.github.com>
2026-02-17 14:03:04 -0600

    Update gameproglotteryD.py

--- gameproglotteryD.py
+++ gameproglotteryD.py
@@ -1,4 +1,4 @@
-#This is a lottery game created by Derrick Hobbs under Creative Common Attribution Non Commercial (CC BY -NC) License#
+#This is a lottery game created by Derrick Hobbs licensed under Creative Common Attribution Non Commercial (CC BY -NC) License Copyright (c) 2026 Derrick Hobbs#
 import random
 a = int(input("What is your lottery number"))
 random_number = random.randint(1,10)
