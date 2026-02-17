56fbbff44d1b6aab9fa6b9298fc10a8d9271101a
dkhobbs <91768310+dkhobbs@users.noreply.github.com>
2026-02-17 13:16:13 -0600

    Add files via upload

---
+++ gameproglotteryD.py
@@ -0,0 +1,18 @@
+#This is a lottery game created by Derrick Hobbs#
+import random
+a = int(input("What is your lottery number"))
+random_number = random.randint(1,10)
+if a == random_number:
+    print(random_number)
+    print("     **  ")
+    print("    *****")
+    print("  **     ")
+    print("  **     ")
+    print("    *****")
+    print("         **")
+    print("         **") 
+    print("    *****")
+    print("     ** ")
+    print("You have won the lottery!!!")
+else:
+    print("Wrong number")
